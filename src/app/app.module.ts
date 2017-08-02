293ad95a43c1b935b2e31388827ac2a2a29c77f1
Manikandan <Manikandan>
2017-08-02 08:11:43 +0530

    initial files

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import { RouterModule }   from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      LoginComponent,
+    HomeComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'login', component:LoginComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
